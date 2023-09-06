1d18351ccfd1f45fe70e3108061c5b9e81b17af5
정서현 <tjgus027@kakao.com>
2023-09-06 21:32:08 +0900

    feat: mapper 파일 생성 및 쿼리 작성

---
+++ src/main/java/com/hobby/chain/member/domain/mapper/MemberMapper.java
@@ -0,0 +1,11 @@
+package com.hobby.chain.member.domain.mapper;
+
+import com.hobby.chain.member.domain.entity.Member;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MemberMapper {
+    int findById(@Param("userId") String userId);
+    void insertMember(Member member);
+}
