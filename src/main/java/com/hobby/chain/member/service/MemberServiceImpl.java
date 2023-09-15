e892d5634e44e6304ec4659d5ee855232dd66570
정서현 <tjgus027@kakao.com>
2023-09-15 16:54:37 +0900

    feat: 비밀번호 암호화

--- src/main/java/com/hobby/chain/member/service/MemberServiceImpl.java
+++ src/main/java/com/hobby/chain/member/service/MemberServiceImpl.java
@@ -4,23 +4,32 @@ import com.hobby.chain.member.domain.entity.Member;
 import com.hobby.chain.member.domain.mapper.MemberMapper;
 import com.hobby.chain.member.dto.MemberDTO;
 import com.hobby.chain.member.exception.DuplicationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class MemberServiceImpl implements MemberService{
     private final MemberMapper memberMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public MemberServiceImpl(MemberMapper memberMapper) {
+    public MemberServiceImpl(MemberMapper memberMapper, PasswordEncoder passwordEncoder) {
         this.memberMapper = memberMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void signUp(MemberDTO memberDTO) {
-        boolean notExistMember = isNotExistMember(findById(memberDTO.getUserId()));
+        boolean notExistMember = isNotExistMember(exist(memberDTO.getUserId()));
+
         if(notExistMember) {
             memberMapper.insertMember(Member.builder()
                             .userId(memberDTO.getUserId())
-                            .password(memberDTO.getPassword())
+                            .password(passwordEncoder.encode(memberDTO.getPassword()))
                             .name(memberDTO.getName())
                             .nickName(memberDTO.getNickName())
                             .phoneNumber(memberDTO.getPhoneNumber())
@@ -32,8 +41,9 @@ public class MemberServiceImpl implements MemberService{
         }
     }
 
-    public int findById(String userId) {
-        return memberMapper.findById(userId);
+    @Override
+    public int exist(String userId) {
+        return memberMapper.exist(userId);
     }
 
     private boolean isNotExistMember(int isYn) {
