bba80bfd3c4a4fbbde9beadb374389b4174a59e5
정서현 <tjgus027@kakao.com>
2023-09-15 19:51:29 +0900

    style: 예외 코드 메시지 추가

--- src/main/java/com/hobby/chain/member/exception/DuplicationException.java
+++ src/main/java/com/hobby/chain/member/exception/DuplicationException.java
@@ -3,6 +3,6 @@ package com.hobby.chain.member.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Already Exists")
 public class DuplicationException extends RuntimeException{
 }
