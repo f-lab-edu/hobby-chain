f956220512e8e539677a5054163bcfa59f6680ab
정서현 <tjgus027@kakao.com>
2023-09-18 16:30:53 +0900

    refactor: 로그인 서비스 인터페이스 분리, 세션 저장 책임 분리 (컨트롤러 -> 서비스)

--- src/main/java/com/hobby/chain/member/controller/MemberController.java
+++ src/main/java/com/hobby/chain/member/controller/MemberController.java
@@ -2,43 +2,43 @@ package com.hobby.chain.member.controller;
 
 import com.hobby.chain.member.dto.MemberDTO;
 import com.hobby.chain.member.dto.MemberLogin;
+import com.hobby.chain.member.service.MemberLoginService;
 import com.hobby.chain.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
 public class MemberController {
     private final MemberService memberService;
+    private final MemberLoginService loginService;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, MemberLoginService loginService) {
         this.memberService = memberService;
+        this.loginService = loginService;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void memberSave(@RequestBody @Valid MemberDTO memberDTO){
+    public void createMember(@RequestBody @Valid MemberDTO memberDTO){
         memberService.signUp(memberDTO);
     }
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    public void memberLogin(@RequestBody MemberLogin memberLogin, HttpSession session){
-        memberService.login(memberLogin.getUserId(), memberLogin.getPassword());
-        session.setAttribute("member", memberLogin.getUserId());
+    public void login(@RequestBody MemberLogin memberLogin){
+        loginService.login(memberLogin.getUserId(), memberLogin.getPassword());
     }
 
     @PostMapping("/logout")
-    public void memberLogout(HttpSession session){
-        session.invalidate();
+    public void logout(){
+        loginService.logout();
     }
 
     @PostMapping("/me")
-    public MemberDTO memberInfo(HttpSession session){
-        String userId = String.valueOf(session.getAttribute("member"));
-        return memberService.getMemberInfo(userId);
+    public MemberDTO getMemberInfo(){
+        return memberService.getMemberInfo();
     }
 }
