e7423482a4d8b6f5df28f7e13b277a039340d350
정서현 <tjgus027@kakao.com>
2023-09-20 15:46:44 +0900

    test: 게시물 업로드 테스트

--- src/main/java/com/hobby/chain/post/domain/mapper/PostMapper.java
+++ src/main/java/com/hobby/chain/post/domain/mapper/PostMapper.java
@@ -4,13 +4,18 @@ import com.hobby.chain.post.dto.ImageDTO;
 import com.hobby.chain.post.dto.PostDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface PostMapper {
     void insertPost(PostDTO postDTO);
     void insertImages(ImageDTO imageDTO);
     boolean isExistsImage(long postId);
-    PostDTO getAllPost();
+    boolean isExistsPost(long memberIdx);
+    List<PostDTO> getAllPost();
     PostDTO getPostWithImage(long postId);
     PostDTO getPostWithoutImage(long postId);
+
+    List<PostDTO> getAllPostWithoutImage();
 
 }
---
+++ src/test/java/com/hobby/chain/post/service/PostServiceImplTest.java
@@ -0,0 +1,78 @@
+package com.hobby.chain.post.service;
+
+import com.hobby.chain.member.Gender;
+import com.hobby.chain.member.domain.entity.Member;
+import com.hobby.chain.member.domain.mapper.MemberMapper;
+import com.hobby.chain.member.dto.MemberDTO;
+import com.hobby.chain.member.service.MemberLoginService;
+import com.hobby.chain.member.service.MemberService;
+import com.hobby.chain.post.domain.mapper.PostMapper;
+import com.hobby.chain.post.dto.ImageDTO;
+import com.hobby.chain.post.dto.PostDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@Transactional
+class PostServiceImplTest {
+    private PostMapper postMapper;
+    private MemberMapper memberMapper;
+
+    private PostService service;
+    private MemberService memberService;
+    private MemberLoginService loginService;
+
+    @Autowired
+    public PostServiceImplTest(PostMapper postMapper, MemberMapper memberMapper, PostService service, MemberService memberService, MemberLoginService loginService) {
+        this.postMapper = postMapper;
+        this.memberMapper = memberMapper;
+        this.service = service;
+        this.memberService = memberService;
+        this.loginService = loginService;
+    }
+
+    private MemberDTO memberDTO;
+    @BeforeEach
+    void testMemberSetUp(){
+        memberDTO = MemberDTO.builder()
+                .userId("qpqp7376@gmail.com")
+                .password("xxxx1234*")
+                .name("정서현")
+                .nickName("서현")
+                .phoneNumber("010-4600-4123")
+                .gender(Gender.M)
+                .birth("20040227")
+                .build();
+        memberService.signUp(memberDTO);
+
+        loginService.login(memberDTO.getUserId(), memberDTO.getPassword());
+    }
+
+    @Test
+    void 게시물_업로드_이미지X(){
+        //given
+        PostDTO postDTO = PostDTO.builder()
+                .userIdx(loginService.getLoginMemberIdx())
+                .post_content("테스트").build();
+
+        //when
+        service.uploadNewPost(postDTO.getUserIdx(), postDTO);
+
+        //then
+        assertThat(postMapper.getAllPostWithoutImage()).isNotNull();
+    }
+}
