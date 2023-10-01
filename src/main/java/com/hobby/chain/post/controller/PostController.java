f40914569c329cb74c0f6eee3c1914faebf0aa45
정서현 <tjgus027@kakao.com>
2023-10-01 23:20:57 +0900

    style: 변수명 변경 (service -> postService)

--- src/main/java/com/hobby/chain/post/controller/PostController.java
+++ src/main/java/com/hobby/chain/post/controller/PostController.java
@@ -6,34 +6,37 @@ import com.hobby.chain.post.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
-    private final PostService service;
+    private final PostService postService;
     private final MemberLoginService loginService;
 
     public PostController(PostService service, MemberLoginService loginService) {
-        this.service = service;
+        this.postService = service;
         this.loginService = loginService;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public void uploadNewPost(@RequestBody PostDTO postDTO){
-        service.uploadNewPost(loginService.getLoginMemberIdx(), postDTO);
+    public void uploadNewPost(@RequestParam String content,
+                              @RequestParam List<MultipartFile> images){
+        postService.uploadNewPost(loginService.getLoginMemberIdx(), content, images);
     }
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<PostDTO> getAllPost(){
-        return service.getAllPost();
+    public List<PostDTO> getPosts(){
+        return postService.getPosts();
     }
 
     @GetMapping("/one")
     @ResponseStatus(HttpStatus.OK)
     public PostDTO getPost(){
-        return service.getPost(loginService.getLoginMemberIdx());
+        return postService.getPost(loginService.getLoginMemberIdx());
     }
+
 }
