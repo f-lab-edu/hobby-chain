9bee36108b1834c4c4e7ed103b4631433c312d98
정서현 <tjgus027@kakao.com>
2023-10-04 21:50:19 +0900

    refactor: 파일 업로드 시 에러 상태 코드 추가

--- src/main/java/com/hobby/chain/post/exception/FileUploadFailException.java
+++ src/main/java/com/hobby/chain/post/exception/FileUploadFailException.java
@@ -1,6 +1,9 @@
 package com.hobby.chain.post.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class FileUploadFailException extends RuntimeException{
     public FileUploadFailException() {
         super();
