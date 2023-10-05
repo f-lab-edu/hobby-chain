b6346b4c1d1011db83db3e7cb78b69b07df51382
정서현 <tjgus027@kakao.com>
2023-10-05 21:22:48 +0900

    test: 회원 정보 조회, 수정, 탈퇴 서비스 코드 테스트

--- src/test/java/com/hobby/chain/member/service/MemberServiceImplTest.java
+++ src/test/java/com/hobby/chain/member/service/MemberServiceImplTest.java
@@ -2,17 +2,18 @@ package com.hobby.chain.member.service;
 
 import com.hobby.chain.member.Gender;
 import com.hobby.chain.member.dto.MemberDTO;
+import com.hobby.chain.member.dto.MemberInfo;
+import com.hobby.chain.member.dto.UpdateRequestInfo;
 import com.hobby.chain.member.exception.DuplicationException;
 import com.hobby.chain.member.exception.ForbiddenException;
-import com.hobby.chain.member.exception.IncorrectPasswordException;
-import com.hobby.chain.member.exception.NotExistUserException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -80,8 +81,6 @@ class MemberServiceImplTest {
         assertThat(de.getClass()).isEqualTo(DuplicationException.class);
     }
 
-
-
     @Test
     void 회원_정보_조회(){
         //given
@@ -94,11 +93,63 @@ class MemberServiceImplTest {
                 .gender(Gender.M)
                 .birth("20040227").build();
         memberService.signUp(memberDTO);
+        loginService.login(memberDTO.getEmail(), memberDTO.getPassword());
 
         //when
-
+        MemberInfo memberInfo = memberService.getMemberInfo();
 
         //then
+        assertThat(memberDTO.getEmail()).isEqualTo(memberInfo.getEmail());
+    }
+
+    @Test
+    void 회원_정보_수정(){
+        //given
+        MemberDTO memberDTO = MemberDTO.builder()
+                .email("qpqp7371@gmail.com")
+                .password("xxxx")
+                .name("정서현")
+                .nickName("서현")
+                .phoneNumber("010-4600-4123")
+                .gender(Gender.M)
+                .birth("20040227").build();
+        memberService.signUp(memberDTO);
+        loginService.login(memberDTO.getEmail(), memberDTO.getPassword());
+
+        UpdateRequestInfo requestInfo = UpdateRequestInfo.builder()
+                .nickName("tjgus")
+                .phoneNumber("010-0000-0000")
+                .gender(Gender.F)
+                .birth(Date.valueOf("2004-02-27")).build();
+
+        //when
+        memberService.updateMemberInfo(requestInfo);
+
+        //then
+        MemberInfo memberInfo = memberService.getMemberInfo();
+        assertThat(memberInfo.getNickName()).isEqualTo(requestInfo.getNickName());
+    }
+
+    @Test
+    void 회원_탈퇴(){
+        //given
+        MemberDTO memberDTO = MemberDTO.builder()
+                .email("qpqp7371@gmail.com")
+                .password("xxxx")
+                .name("정서현")
+                .nickName("서현")
+                .phoneNumber("010-4600-4123")
+                .gender(Gender.M)
+                .birth("20040227").build();
+        memberService.signUp(memberDTO);
+        loginService.login(memberDTO.getEmail(), memberDTO.getPassword());
+
+        //when
+        memberService.deleteMember();
+        ForbiddenException fe = assertThrows(ForbiddenException.class, () -> memberService.getMemberInfo());
+
+        //then
+        assertThat(fe.getClass()).isEqualTo(ForbiddenException.class);
     }
 
 }
