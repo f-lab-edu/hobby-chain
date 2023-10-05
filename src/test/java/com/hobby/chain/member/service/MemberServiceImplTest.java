da14aa031aea8838f6b48bcf180616b5eb36ce52
정서현 <tjgus027@kakao.com>
2023-10-05 19:38:53 +0900

    test: 테스트 코드 클래스 분리 (중복 코드 제거)

--- src/test/java/com/hobby/chain/member/service/MemberServiceImplTest.java
+++ src/test/java/com/hobby/chain/member/service/MemberServiceImplTest.java
@@ -3,6 +3,7 @@ package com.hobby.chain.member.service;
 import com.hobby.chain.member.Gender;
 import com.hobby.chain.member.dto.MemberDTO;
 import com.hobby.chain.member.exception.DuplicationException;
+import com.hobby.chain.member.exception.ForbiddenException;
 import com.hobby.chain.member.exception.IncorrectPasswordException;
 import com.hobby.chain.member.exception.NotExistUserException;
 import org.junit.jupiter.api.Assertions;
@@ -21,13 +22,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberServiceImplTest {
 
     private final MemberService memberService;
-    private PasswordEncoder passwordEncoder;
     private final MemberLoginService loginService;
 
     @Autowired
-    public MemberServiceImplTest(MemberService memberService, PasswordEncoder passwordEncoder, MemberLoginService loginService) {
+    public MemberServiceImplTest(MemberService memberService, MemberLoginService loginService) {
         this.memberService = memberService;
-        this.passwordEncoder = passwordEncoder;
         this.loginService = loginService;
     }
 
@@ -81,11 +80,13 @@ class MemberServiceImplTest {
         assertThat(de.getClass()).isEqualTo(DuplicationException.class);
     }
 
+
+
     @Test
-    void 로그인_성공_테스트(){
+    void 회원_정보_조회(){
         //given
         MemberDTO memberDTO = MemberDTO.builder()
-                .email("qpqp7377@gmail.com")
+                .email("qpqp7371@gmail.com")
                 .password("xxxx")
                 .name("정서현")
                 .nickName("서현")
@@ -94,71 +95,10 @@ class MemberServiceImplTest {
                 .birth("20040227").build();
         memberService.signUp(memberDTO);
 
-        String userId = "qpqp7377@gmail.com";
-        String pwd = "xxxx";
-
         //when
-        loginService.login(userId, pwd);
+
 
         //then
-        assertThat(userId).isEqualTo(memberService.getMemberInfo().getEmail());
-    }
-
-    @Test
-    void 로그인_실패_아이디X(){
-        //given
-        MemberDTO memberDTO = MemberDTO.builder()
-                .email("r2gards1@gmail.com")
-                .password("xxxx")
-                .name("정서현")
-                .nickName("서현")
-                .phoneNumber("010-4600-4123")
-                .gender(Gender.M)
-                .birth("20040227").build();
-        memberService.signUp(memberDTO);
-
-        String userId = "r2gards2@gmail.com";
-        String pwd = "xxxx";
-
-        //when
-        NotExistUserException ne = assertThrows(NotExistUserException.class,
-                () -> loginService.login(userId, pwd));
-
-        //then
-        assertThat(ne.getClass()).isEqualTo(NotExistUserException.class);
-    }
-
-    @Test
-    void 로그인_실패_비밀번호_일치X(){
-        //given
-        MemberDTO memberDTO = MemberDTO.builder()
-                .email("qpqp7377@gmail.com")
-                .password("xxxx")
-                .name("정서현")
-                .nickName("서현")
-                .phoneNumber("010-4600-4123")
-                .gender(Gender.M)
-                .birth("20040227").build();
-        memberService.signUp(memberDTO);
-
-        String userId = "qpqp7374@gmail.com";
-        String pwd = "xdre12";
-
-        //when
-        IncorrectPasswordException ie = assertThrows(IncorrectPasswordException.class,
-                () -> loginService.login(userId, pwd));
-
-        //then
-        assertThat(ie.getClass()).isEqualTo(IncorrectPasswordException.class);
-    }
-
-    @Test
-    void 아이디_얻기_로그인X() {
-        //when
-        NullPointerException ne = assertThrows(NullPointerException.class, () -> loginService.getLoginMemberIdx());
-
-        //then
-        assertThat(ne.getClass()).isEqualTo(NullPointerException.class);
     }
 
 }
