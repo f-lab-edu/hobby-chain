60563dadf8756b4f00678eda714c8af14ea0e9a1
정서현 <tjgus027@kakao.com>
2023-10-09 22:37:12 +0900

    refactor: 생성자 추가 (기본, 메시지 포함)

--- src/main/java/com/hobby/chain/member/exception/ForbiddenException.java
+++ src/main/java/com/hobby/chain/member/exception/ForbiddenException.java
@@ -5,4 +5,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Not Authorized")
 public class ForbiddenException extends RuntimeException{
+    public ForbiddenException() {
+        super();
+    }
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
 }
