18d5b5b960ac02930b8e80f7b8a741422259faa8
정서현 <tjgus027@kakao.com>
2023-10-14 19:20:36 +0900

    refactor: 컨트롤러 파라미터 수정 (follower Id 제거)

--- src/main/java/com/hobby/chain/follow/controller/FollowController.java
+++ src/main/java/com/hobby/chain/follow/controller/FollowController.java
@@ -1,24 +1,27 @@
 package com.hobby.chain.follow.controller;
 
 import com.hobby.chain.follow.service.FollowService;
+import com.hobby.chain.member.service.MemberLoginService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/follows")
 public class FollowController {
     private final FollowService followService;
+    private final MemberLoginService loginService;
 
-    public FollowController(FollowService followService) {
+    public FollowController(FollowService followService, MemberLoginService loginService) {
         this.followService = followService;
+        this.loginService = loginService;
     }
 
     @PostMapping
-    public void subscribeUser(@RequestParam("userId") long follower, @RequestParam("followeeId") long followee){
-        followService.subscribe(follower, followee);
+    public void subscribeUser(@RequestParam("followeeId") long followee){
+        followService.subscribe(loginService.getLoginMemberIdx(), followee);
     }
 
     @DeleteMapping
-    public void unsubscribe(@RequestParam("userId") long follower, @RequestParam("followeeId") long followee){
-        followService.unsubscribe(follower, followee);
+    public void unsubscribe(@RequestParam("followeeId") long followee){
+        followService.unsubscribe(loginService.getLoginMemberIdx(), followee);
     }
 }
