64e3c3a0f3e12aba75817cfeeb06e8065b96ae12
정서현 <tjgus027@kakao.com>
2023-10-17 19:25:03 +0900

    test: 파일 업로드 정상 테스트

--- src/test/java/com/hobby/chain/post/service/FileServiceImplTest.java
+++ src/test/java/com/hobby/chain/post/service/FileServiceImplTest.java
@@ -2,7 +2,6 @@ package com.hobby.chain.post.service;
 
 import com.hobby.chain.post.dto.ImageDTO;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,17 +22,13 @@ class FileServiceImplTest {
         this.fileService = fileService;
     }
 
-    @BeforeEach
-    void imgSetUp() throws Exception {
+    @Test
+    void 파일_업로드_정상_테스트() throws Exception{
+        //given
         MultipartFile file = new MockMultipartFile("image", "test.png", "image/png",
                 new FileInputStream("/Users/mac/Downloads/test.png"));
         files = new ArrayList<>();
         files.add(file);
-    }
-
-    @Test
-    void 파일_업로드_테스트(){
-        //given
         long postId = 1L;
 
         //when
