aa3b98d1e7d02a6c21c6ed7eec9f92cb82113307
정서현 <tjgus027@kakao.com>
2023-11-23 17:15:24 +0900

    Fix: 파라미터 회원 객체로 수정 (비회원이 접근하는 API에 Long 타입 포함 시 잘못된 로직타는 부분 수정)

---
+++ src/main/java/com/hobby/chain/member/dto/MemberId.java
@@ -0,0 +1,8 @@
+package com.hobby.chain.member.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberId {
+    private Long userId;
+}
--- src/main/java/com/hobby/chain/resolver/LoginUserArgumentResolver.java
+++ src/main/java/com/hobby/chain/resolver/LoginUserArgumentResolver.java
@@ -1,5 +1,6 @@
 package com.hobby.chain.resolver;
 
+import com.hobby.chain.member.dto.MemberId;
 import com.hobby.chain.member.exception.ForbiddenException;
 import com.hobby.chain.util.SessionKey;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +20,7 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.getParameter().equals(Long.class);
+        return parameter.getParameter().equals(MemberId.class);
     }
 
     @Override
